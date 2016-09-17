0f977824b736b8777c47f8893315932810bb019b
Shafique Jamal <shafique.jamal@gmail.com>
2016-09-17 10:12:11 -0400

    Add initial quantity to acquire calculator

--- src/main/scala/com/eigenroute/portfolioanalysis/PortfolioRebalancer.scala
+++ src/main/scala/com/eigenroute/portfolioanalysis/PortfolioRebalancer.scala
@@ -3,12 +3,12 @@ package com.eigenroute.portfolioanalysis
 case class PortfolioValueDifference(eTFCode: ETFCode, valueDifference: Double)
 case class PortfolioWeightDifference(eTFCode: ETFCode, weightDifference: Double)
 case class ETFDesiredValue(eTFCode: ETFCode, value:Double, isToTrade: Boolean)
-
-case class PortfolioValueDifferences()
+case class PorfolioQuanitiesToAcquire(
+            eTFCode: ETFCode, quantityToAcquire:Int, effectivePrice: Double, fractionalQuantity: Double)
 
 class PortfolioRebalancer {
 
-  def weightDifference(
+  def weightDifferences(
     portfolioDesign: PortfolioDesign,
     portfolioSnapshot: PortfolioSnapshot):Seq[PortfolioWeightDifference] = {
 
@@ -23,7 +23,7 @@ class PortfolioRebalancer {
 
   }
 
-  def newDesiredValue(
+  def newDesiredValues(
     portfolioDesign: PortfolioDesign,
     weightDifferences: Seq[PortfolioWeightDifference],
     portfolioSnapshot: PortfolioSnapshot,
@@ -61,7 +61,7 @@ class PortfolioRebalancer {
     valueOfETFsToTrade ++ valueOfETFsNotToTrade
   }
 
-  def valueDifference(
+  def valueDifferences(
     desiredValues:Seq[ETFDesiredValue],
     portfolioSnapshot: PortfolioSnapshot): Seq[PortfolioValueDifference] =
     desiredValues.map { desiredValue =>
@@ -74,6 +74,27 @@ class PortfolioRebalancer {
         PortfolioValueDifference(desiredValue.eTFCode, 0)
       }
     }
+
+  def firstEstimateQuantitiesToAcquire(
+    portfolioDesign: PortfolioDesign,
+    portfolioSnapshot: PortfolioSnapshot,
+    valueDifferences: Seq[PortfolioValueDifference],
+    bidAskCostFractionOfNAV: Double): Seq[PorfolioQuanitiesToAcquire] = {
+
+    def price(valueDifference:Double, nAV: Double):Double =
+      if (valueDifference > 0)
+        nAV * (1 + bidAskCostFractionOfNAV)
+      else
+        nAV / (1 + bidAskCostFractionOfNAV)
+
+    valueDifferences.map { pVD =>
+      val nAV: Double = portfolioSnapshot.eTFDatas.find(_.eTFCode == pVD.eTFCode).map(_.nAV).get //TODO: do something about the get
+      val effectivePrice: Double = price(pVD.valueDifference, nAV)
+      val quantity: Double = pVD.valueDifference / effectivePrice
+      PorfolioQuanitiesToAcquire(pVD.eTFCode, math.floor(quantity).toInt, effectivePrice, quantity)
+    }
+
+  }
 
   def portfolioValue(portfolioSnapshot: PortfolioSnapshot): Double = portfolioValueFromETFDatas(portfolioSnapshot.eTFDatas)
 
--- src/test/scala/com/eigenroute/portfolioanalysis/PortfolioRebalancerUTest.scala
+++ src/test/scala/com/eigenroute/portfolioanalysis/PortfolioRebalancerUTest.scala
@@ -5,90 +5,13 @@ import java.sql.Timestamp
 import org.joda.time.DateTime
 import org.scalatest.{FlatSpec, ShouldMatchers}
 
-class PortfolioRebalancerUTest extends FlatSpec with ShouldMatchers {
+class PortfolioRebalancerUTest extends FlatSpec with ShouldMatchers with PortfolioFixture {
 
-  val now = new Timestamp(new DateTime(2016, 1, 1, 1, 1, 1).getMillis)
-
-  val eTFA = ETFCode("AAA")
-  val eTFB = ETFCode("BBB")
-  val eTFC = ETFCode("CCC")
-  val eTFD = ETFCode("DDD")
-  val eTFASelection = ETFSelection(eTFA, 0.25)
-  val eTFBSelection = ETFSelection(eTFB, 0.5)
-  val eTFCSelection = ETFSelection(eTFC, 0.1)
-  val eTFDSelection = ETFSelection(eTFD, 0.15)
-  val portfolioDesign = PortfolioDesign(Seq(eTFASelection, eTFBSelection, eTFCSelection, eTFDSelection))
-
-  val eTFDataPlusA = ETFDataPlus(now, eTFA, "", 20, 0, 50, 0d)
-  val eTFDataPlusB = ETFDataPlus(now, eTFB, "", 30, 0, 100, 0d)
-  val eTFDataPlusC = ETFDataPlus(now, eTFC, "", 40, 0, 100, 0d)
-  val eTFDataPlusD = ETFDataPlus(now, eTFD, "", 50, 0, 40, 0d)
-  val portfolioSnapshot = PortfolioSnapshot(Seq(eTFDataPlusA, eTFDataPlusB, eTFDataPlusC, eTFDataPlusD))
-
-  val weightDifferences = Seq(
-     PortfolioWeightDifference(eTFA, 0.15),
-     PortfolioWeightDifference(eTFB, 0.2),
-     PortfolioWeightDifference(eTFC, -0.3),
-     PortfolioWeightDifference(eTFD, -0.05)
-  )
-
-  val pm = new PortfolioRebalancer
-
-  trait DesiredValueFixture {
-    val expectedDesiredValuesOneToBeTraded = Seq(
-      ETFDesiredValue(eTFA, 2355.88235, isToTrade = true),
-      ETFDesiredValue(eTFB, 4711.76471, isToTrade = true),
-      ETFDesiredValue(eTFC, 942.35294, isToTrade = true),
-      ETFDesiredValue(eTFD, 2000.00000, isToTrade = false)
-    )
-
-    val expectedDesiredValuesAllToBeTraded = Seq(
-       ETFDesiredValue(eTFA, 2500d, isToTrade = true),
-       ETFDesiredValue(eTFB, 5000d, isToTrade = true),
-       ETFDesiredValue(eTFC, 1000d, isToTrade = true),
-       ETFDesiredValue(eTFD, 1500d, isToTrade = true)
-    )
-
-    val expectedDesiredValuesAllToBeTradedcost15ExDivCash100 = Seq(
-      ETFDesiredValue(eTFA, 2510d, isToTrade = true),
-      ETFDesiredValue(eTFB, 5020d, isToTrade = true),
-      ETFDesiredValue(eTFC, 1004d, isToTrade = true),
-      ETFDesiredValue(eTFD, 1506d, isToTrade = true)
-    )
-
-    val expectedDesiredValuesNoTrades = Seq(
-      ETFDesiredValue(eTFA, 1000d, isToTrade = false),
-      ETFDesiredValue(eTFB, 3000d, isToTrade = false),
-      ETFDesiredValue(eTFC, 4000d, isToTrade = false),
-      ETFDesiredValue(eTFD, 2000d, isToTrade = false)
-    )
-
-    val expectedValueDifferenceOneTrade = Seq(
-      PortfolioValueDifference(eTFA, 1355.88235),
-      PortfolioValueDifference(eTFB, 1711.76471),
-      PortfolioValueDifference(eTFC, -3057.64706),
-      PortfolioValueDifference(eTFD, 0.00000)
-    )
-
-    val expectedValueDifferenceNoTrades = Seq(
-      PortfolioValueDifference(eTFA, 0d),
-      PortfolioValueDifference(eTFB, 0d),
-      PortfolioValueDifference(eTFC, 0d),
-      PortfolioValueDifference(eTFD, 0d)
-    )
-
-    val expectedValueDifferenceAllTrades = Seq(
-      PortfolioValueDifference(eTFA, 1500d),
-      PortfolioValueDifference(eTFB, 2000d),
-      PortfolioValueDifference(eTFC, -3000d),
-      PortfolioValueDifference(eTFD, -500d)
-    )
-  }
+  val pr = new PortfolioRebalancer
 
   "The weight difference calculator" should "accurately calculate the weight difference" in {
-    pm.weightDifference(portfolioDesign, portfolioSnapshot).map { portfolioWeightDifference =>
-      PortfolioWeightDifference(portfolioWeightDifference.eTFCode,
-        BigDecimal(portfolioWeightDifference.weightDifference).setScale(5, BigDecimal.RoundingMode.HALF_UP).toDouble)
+    pr.weightDifferences(portfolioDesign, portfolioSnapshot).map { portfolioWeightDifference =>
+      PortfolioWeightDifference(portfolioWeightDifference.eTFCode, round(portfolioWeightDifference.weightDifference))
     } should contain theSameElementsAs weightDifferences
   }
 
@@ -106,39 +29,90 @@ class PortfolioRebalancerUTest extends FlatSpec with ShouldMatchers {
     checkNewDesiredValue(1d, expectedDesiredValuesNoTrades, 10d, 20d, 20d)
   }
 
-  def checkNewDesiredValue(
+  private def checkNewDesiredValue(
     maxAllowedDeviation: Double,
     expected: Seq[ETFDesiredValue],
     perETFTradingCost: Double,
     accExDiv: Double,
     accCash: Double): Unit = {
-    pm.newDesiredValue(
-        portfolioDesign, weightDifferences, portfolioSnapshot, maxAllowedDeviation, perETFTradingCost, accExDiv, accCash
-      ).map { dV =>
-        ETFDesiredValue(dV.eTFCode, BigDecimal(dV.value).setScale(5, BigDecimal.RoundingMode.HALF_UP).toDouble, dV.isToTrade)
+    pr.newDesiredValues(
+        portfolioDesign, weightDifferences, portfolioSnapshot, maxAllowedDeviation, perETFTradingCost, accExDiv, accCash)
+        .map { dV => ETFDesiredValue(dV.eTFCode, round(dV.value), dV.isToTrade)
     } should contain theSameElementsAs expected
   }
 
   "The value difference calculator" should "calculate the difference in value as non-zero for ETFs to be traded, and zero " +
   "for ETFs not to be traded, and should get the signs correct" in new DesiredValueFixture {
 
-    pm.valueDifference(expectedDesiredValuesOneToBeTraded, portfolioSnapshot).map { vDiff =>
-      PortfolioValueDifference(vDiff.eTFCode,
-        BigDecimal(vDiff.valueDifference).setScale(5, BigDecimal.RoundingMode.HALF_UP).toDouble)
+    pr.valueDifferences(expectedDesiredValuesOneToBeTraded, portfolioSnapshot).map { vDiff =>
+      PortfolioValueDifference(vDiff.eTFCode, round(vDiff.valueDifference))
     } should contain theSameElementsAs expectedValueDifferenceOneTrade
 
-    pm.valueDifference(expectedDesiredValuesNoTrades, portfolioSnapshot) should
+    pr.valueDifferences(expectedDesiredValuesNoTrades, portfolioSnapshot) should
       contain theSameElementsAs expectedValueDifferenceNoTrades
 
-    pm.valueDifference(expectedDesiredValuesAllToBeTraded, portfolioSnapshot) should
+    pr.valueDifferences(expectedDesiredValuesAllToBeTraded, portfolioSnapshot) should
       contain theSameElementsAs expectedValueDifferenceAllTrades
   }
 
   "The portfolio value calculator" should "calculate the value of the portfolio using the nav and quantity from the " +
   "snapshot" in {
 
-    pm.portfolioValue(portfolioSnapshot) shouldEqual 10000d
-    pm.portfolioValueFromETFDatas(portfolioSnapshot.eTFDatas) shouldEqual 10000d
+    pr.portfolioValue(portfolioSnapshot) shouldEqual 10000d
+    pr.portfolioValueFromETFDatas(portfolioSnapshot.eTFDatas) shouldEqual 10000d
 
   }
+
+  "The quantity difference calculator" should "calculate the new quantities to be purchased such that as much free cash as" +
+  "possible is invested, with the minimum deviation from the desired weights" in new DesiredValueFixture {
+
+    val expectedFirstEstimateQuantitiesAllTrades = Seq(
+      PorfolioQuanitiesToAcquire(eTFA, 74, round(20*(1 + 0.0011)), 74.91759),
+      PorfolioQuanitiesToAcquire(eTFB, 66, round(30*(1 + 0.0011)), 66.59341),
+      PorfolioQuanitiesToAcquire(eTFC, -76, round(40/(1 + 0.0011)), -75.0825),
+      PorfolioQuanitiesToAcquire(eTFD, -11, round(50/(1 + 0.0011)), -10.011)
+    )
+
+    val expectedFirstEstimateQuantitiesAllTradesExpensive = Seq(
+      PorfolioQuanitiesToAcquire(eTFA, 74, round(20*(1 + 0.0025)), 74.81297),
+      PorfolioQuanitiesToAcquire(eTFB, 66, round(30*(1 + 0.0025)), 66.50042),
+      PorfolioQuanitiesToAcquire(eTFC, -76, round(40/(1 + 0.0025)), -75.1875),
+      PorfolioQuanitiesToAcquire(eTFD, -11, round(50/(1 + 0.0025)), -10.025)
+    )
+
+    val expectedFirstEstimateQuantitiesOneTrade = Seq(
+      PorfolioQuanitiesToAcquire(eTFA, 67, round(20*(1 + 0.0011)), 67.71963),
+      PorfolioQuanitiesToAcquire(eTFB, 56, round(30*(1 + 0.0011)), 56.99613),
+      PorfolioQuanitiesToAcquire(eTFC, -77, round(40/(1 + 0.0011)), -76.52526),
+      PorfolioQuanitiesToAcquire(eTFD, 0, round(50/(1 + 0.0011)), 0)
+    )
+
+    val expectedFirstEstimateQuantitiesNoTrades = Seq(
+      PorfolioQuanitiesToAcquire(eTFA, 0, round(20/(1 + 0.0011)), 0),
+      PorfolioQuanitiesToAcquire(eTFB, 0, round(30/(1 + 0.0011)), 0),
+      PorfolioQuanitiesToAcquire(eTFC, 0, round(40/(1 + 0.0011)), 0),
+      PorfolioQuanitiesToAcquire(eTFD, 0, round(50/(1 + 0.0011)), 0)
+    )
+
+    checkFirstEstimateQuantitiesToAcquire(expectedValueDifferenceAllTrades, expectedFirstEstimateQuantitiesAllTrades)
+    checkFirstEstimateQuantitiesToAcquire(expectedValueDifferenceAllTrades, expectedFirstEstimateQuantitiesAllTradesExpensive, 0.0025)
+    checkFirstEstimateQuantitiesToAcquire(expectedValueDifferenceOneTrade, expectedFirstEstimateQuantitiesOneTrade)
+    checkFirstEstimateQuantitiesToAcquire(expectedValueDifferenceNoTrades, expectedFirstEstimateQuantitiesNoTrades)
+
+  }
+
+  private def checkFirstEstimateQuantitiesToAcquire(
+      expectedValueDifference: Seq[PortfolioValueDifference],
+      expectedFirstEstimateQuantities: Seq[PorfolioQuanitiesToAcquire],
+      bidAskCost: Double = 0.0011) {
+
+    pr.firstEstimateQuantitiesToAcquire(portfolioDesign, portfolioSnapshot, expectedValueDifference, bidAskCost)
+    .map { est =>
+      PorfolioQuanitiesToAcquire(
+        est.eTFCode, est.quantityToAcquire, round(est.effectivePrice), round(est.fractionalQuantity) ) } should
+    contain theSameElementsAs expectedFirstEstimateQuantities
+
+  }
+
+  def round(double: Double): Double = BigDecimal(double).setScale(5, BigDecimal.RoundingMode.HALF_UP).toDouble
 }
