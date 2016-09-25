e65047c407a29300750e1e68094b04e658008701
Shafique Jamal <shafique.jamal@gmail.com>
2016-09-25 17:35:43 -0400

    Refactor to call desired values calculator from within value differences calculator

--- src/main/scala/com/eigenroute/portfolioanalysis/rebalancing/ValueDifferencesCalculator.scala
+++ src/main/scala/com/eigenroute/portfolioanalysis/rebalancing/ValueDifferencesCalculator.scala
@@ -1,19 +1,28 @@
 package com.eigenroute.portfolioanalysis.rebalancing
 
-class ValueDifferencesCalculator extends PortfolioValueCalculation {
+class ValueDifferencesCalculator(
+    desiredValuesCalculator: NewDesiredValuesCalculator = new NewDesiredValuesCalculator())
+  extends PortfolioValueCalculation {
 
   def valueDifferences(
-      desiredValues:Seq[ETFDesiredValue],
-      portfolioSnapshot: PortfolioSnapshot): Seq[PortfolioValueDifference] =
+      portfolioDesign: PortfolioDesign,
+      portfolioSnapshot: PortfolioSnapshot,
+      maxAllowedDeviation: Double,
+      perETFTradingCost: Double,
+      accumulatedExDividends: Double,
+      accumulatedCash: Double): Seq[PortfolioValueDifference] = {
+    val desiredValues =
+      desiredValuesCalculator.newDesiredValues(
+        portfolioDesign, portfolioSnapshot, maxAllowedDeviation, perETFTradingCost, accumulatedExDividends, accumulatedCash)
     desiredValues.map { desiredValue =>
       if (desiredValue.isToTrade) {
         val currentValue =
           portfolioSnapshot.sameDateUniqueCodesETFDatas.find(_.eTFCode == desiredValue.eTFCode)
-          .map( eTFData => eTFData.nAV * eTFData.quantity).getOrElse(0d)
+          .map(eTFData => eTFData.nAV * eTFData.quantity).getOrElse(0d)
         PortfolioValueDifference(desiredValue.eTFCode, desiredValue.value - currentValue)
       } else {
         PortfolioValueDifference(desiredValue.eTFCode, 0)
       }
     }
-
+  }
 }
--- src/test/scala/com/eigenroute/portfolioanalysis/rebalancing/ValueDifferencesCalculatorUTest.scala
+++ src/test/scala/com/eigenroute/portfolioanalysis/rebalancing/ValueDifferencesCalculatorUTest.scala
@@ -1,30 +1,43 @@
 package com.eigenroute.portfolioanalysis.rebalancing
 
 import com.eigenroute.portfolioanalysis.PortfolioFixture
+import org.scalamock.scalatest.MockFactory
 import org.scalatest._
 
-class ValueDifferencesCalculatorUTest extends FlatSpec with ShouldMatchers with PortfolioFixture {
+class ValueDifferencesCalculatorUTest extends FlatSpec with ShouldMatchers with PortfolioFixture with MockFactory {
 
-  val valueDifferencesCalculator = new ValueDifferencesCalculator()
+  trait Fixture {
+    val mockCalculator = mock[NewDesiredValuesCalculator]
+    val valueDifferencesCalculator = new ValueDifferencesCalculator(mockCalculator)
+  }
 
   "The value difference calculator" should "calculate the difference in value as non-zero for ETFs to be traded, and zero " +
-  "for ETFs not to be traded, and should get the signs correct" in new DesiredValueFixture {
+  "for ETFs not to be traded, and should get the signs correct" in new DesiredValueFixture with Fixture {
 
-    valueDifferencesCalculator.valueDifferences(expectedDesiredValuesOneToBeTraded, portfolioSnapshot).map { vDiff =>
+    (mockCalculator.newDesiredValues _)
+    .expects(portfolioDesign, portfolioSnapshot, 0.05, 10d, 0d, 0d).returning(expectedDesiredValuesOneToBeTraded)
+    valueDifferencesCalculator.valueDifferences(portfolioDesign, portfolioSnapshot, 0.05, 10d, 0d, 0d).map { vDiff =>
       PortfolioValueDifference(vDiff.eTFCode, round(vDiff.valueDifference))
     } should contain theSameElementsAs expectedValueDifferenceOneTrade
 
-    valueDifferencesCalculator.valueDifferences(expectedDesiredValuesNoTrades, portfolioSnapshot) should
+    (mockCalculator.newDesiredValues _)
+    .expects(portfolioDesign, portfolioSnapshot, 1d, 10d, 0d, 0d).returning(expectedDesiredValuesNoTrades)
+    valueDifferencesCalculator.valueDifferences(portfolioDesign, portfolioSnapshot, 1d, 10d, 0d, 0d) should
     contain theSameElementsAs expectedValueDifferenceNoTrades
 
-    valueDifferencesCalculator.valueDifferences(expectedDesiredValuesAllToBeTraded, portfolioSnapshot) should
+    (mockCalculator.newDesiredValues _)
+    .expects(portfolioDesign, portfolioSnapshot, 0d, 10d, 0d, 0d).returning(expectedDesiredValuesAllToBeTraded)
+    valueDifferencesCalculator.valueDifferences(portfolioDesign, portfolioSnapshot, 0d, 10d, 0d, 0d) should
     contain theSameElementsAs expectedValueDifferenceAllTrades
   }
 
-  it should "correclty calculate the desired value difference for the initial trade" in new DesiredValueFixture {
+  it should "correctly calculate the desired value difference for the initial trade" in new DesiredValueFixture
+  with Fixture {
 
+    (mockCalculator.newDesiredValues _)
+    .expects(portfolioDesign, portfolioSnapshotZeroQuantity, 0d, 10d, 0d, 10040d).returning(expectedDesiredValuesFirstTrades)
     valueDifferencesCalculator
-    .valueDifferences(expectedDesiredValuesFirstTrades, portfolioSnapshotZeroQuantity).map { vDiff =>
+    .valueDifferences(portfolioDesign, portfolioSnapshotZeroQuantity, 0d, 10d, 0d, 10040d).map { vDiff =>
       PortfolioValueDifference(vDiff.eTFCode, round(vDiff.valueDifference))
     } should contain theSameElementsAs expectedValueDifferenceFirstTrades
 
